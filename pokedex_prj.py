25f84fa18ccaa00e5e512d021056603afc33810c
Unixprime7 <michaeld801@nycstudents.net>
2026-02-26 09:24:10 -0500

    Update pokedex_prj.py

--- pokedex_prj.py
+++ pokedex_prj.py
@@ -4,3 +4,13 @@ pokedex = open("./pokedex.json", encoding="utf8")
 ## create variable "data" that represents the enitre pokedex list
 data = json.load(pokedex)
 
+pokemon = input("What pokemon would you like to choose? ")
+language = input("What language would you like it translated to? ")
+
+names = [cheese['name'] for cheese in data]
+
+print(data[names.index(pokemon)][language])
+
+
+
+
