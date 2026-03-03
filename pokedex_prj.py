477ac3e60b9d1a54a364170393bd3d0e2c9199a3
Unixprime7 <michaeld801@nycstudents.net>
2026-03-03 09:26:45 -0500

    Update pokedex_prj.py

--- pokedex_prj.py
+++ pokedex_prj.py
@@ -3,6 +3,8 @@ import json
 pokedex = open("./pokedex.json", encoding="utf8")
 ## create variable "data" that represents the enitre pokedex list
 data = json.load(pokedex)
+types = open("./types.json", encoding="utf8")
+type = json.load(types)
 
 def func1():
     pokemon_number = input("What number pokemon would you like to choose? ")
@@ -14,7 +16,19 @@ def func1():
     print(data[names.index(pokemon)]['name'][language])
 #func1()
 
-def pokesearch():
+def pokesearch(data2, type2):
     poketype = input("What types of pokemon do you want to see? ")
-    
+    types_found = 0
+    for cheese in data2:
+        if poketype in cheese['type']:
+            print(cheese['name']['english'])
+            types_found = 1
+        else:
+            if poketype not in type2['english']:
+                types_found == 0
+                return
+    if types_found == 0:
+            print('This type does not exist')
+        
+pokesearch(data, type)
 
