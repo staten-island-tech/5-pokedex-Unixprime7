06be48f406165976d8b805b82d95f4cf65084646
Unixprime7 <michaeld801@nycstudents.net>
2026-03-04 16:59:18 -0500

    Update pokedex_prj.py

--- pokedex_prj.py
+++ pokedex_prj.py
@@ -1,34 +1,47 @@
 import json
 ## Open the JSON file of pokemon data
-pokedex = open("./pokedex.json", encoding="utf8")
+pokedex_file = open("./pokedex.json", encoding="utf8")
 ## create variable "data" that represents the enitre pokedex list
-data = json.load(pokedex)
-types = open("./types.json", encoding="utf8")
-type = json.load(types)
+pokedex = json.load(pokedex_file)
+types_file = open("./types.json", encoding="utf8")
+types = json.load(types_file)
 
 def func1():
     pokemon_number = input("What number pokemon would you like to choose? ")
     pokemon = int(pokemon_number)
     language = input("What language would you like it translated to? ")
 
-    names = [cheese['id'] for cheese in data]
+    names = [cheese['id'] for cheese in pokedex]
 
-    print(data[names.index(pokemon)]['name'][language])
+    print(pokedex[names.index(pokemon)]['name'][language])
 #func1()
 
-def pokesearch(data2):
-    y = 0
-    poketype = input("What types of pokemon do you want to see? ")
-    for t in data2['type']:
-        if poketype in t:
-            y = 1
-            return
-    if y == 0:
-        print('This type does not exist')
-    if y == 1:     
-        for cheese in data2:
-            if poketype in cheese['type']:
-                print(cheese['name']['english'])
-        
-pokesearch(data)
+
+
+def is_valid_type(search_type):
+
+    for type_object in types:
+        english_type = type_object.get("english")
+        if search_type == english_type:
+            return True
+            
+    return False
+
+def search_pokedex(search_type):
+    
+    for pokemon in pokedex:
+        if search_type in pokemon['type']:
+            print(pokemon['name']['english'])
+        else:
+            pass
+
+user_type = input("What types of pokemon do you want to see? ")
+
+def func2():
+    valid = is_valid_type(user_type)
+    if valid == True:
+        search_pokedex(user_type)
+    else:
+        print("This type does not exist")
+func2()
 
