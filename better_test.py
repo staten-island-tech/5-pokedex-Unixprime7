29e91e8aa55af4f0e4d6cd6041938e5d8053b2d4
Unixprime7 <michaeld801@nycstudents.net>
2026-03-13 09:26:29 -0400

    hi

---
+++ better_test.py
@@ -0,0 +1,10 @@
+def gambling(quarters, one, two, three):
+    while quarters > 0:
+        
+
+
+
+
+
+
+gambling(48, 3, 10, 4)
--- test.py
+++ test.py
@@ -1,10 +1,35 @@
-quarters = int(input("Amount of Martha's quarters""\n"))
-times_played = 0
-one_played = 0
-three_played = 0
-def gambling(mon,one,two,tre):
-    while mon >= 0.25:
+
+def gambling():
+    quarters = input("Amount of Martha's quarters""\n")
+    one_played = input("Machine 1 Played""\n")
+    two_played = input("Machine 2 Played""\n")
+    three_played = input("Machine 3 Played""\n")
+    times_played = 0
+    while quarters > 0:
         one_over = False
         two_over = False
         three_over = False
-        while 
+        while one_over == False:
+            one_played += 1
+            times_played += 1
+            if one_played == 35:
+                one_played = 0
+                quarters += 30
+                one_over = True
+        while two_over == False:
+            two_played += 1
+            times_played += 1
+            if two_played == 100:
+                two_played = 0
+                quarters += 60
+                two_over = True
+        while three_over == False:
+            three_played += 1
+            times_played += 1
+            if three_played == 10:
+                three_played = 0
+                quarters += 9
+                three_over = True
+    print("Martha went broke after", times_played, "times of playing")
+
+gambling()
